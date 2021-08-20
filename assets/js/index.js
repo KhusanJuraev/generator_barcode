1ad49048315694b09eed8815720f166b5d8a9199
Khusan Juraev <56759913+KhusanJuraev@users.noreply.github.com>
2021-08-20 08:30:08 +0500

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,7 +58,7 @@ function addTextToCanvas(text) {
         let ctx = item.getContext('2d')
         ctx.fillStyle = '#fff';
         ctx.fillRect(-300, 10, item.width + 200, item.height / 10);
-        ctx.font = 'regular 16px monospace';
+        ctx.font = 'regular 15px monospace';
         ctx.fillStyle = '#000';
         ctx.textAlign = 'center';
         ctx.fillText(text, -90, 20);
