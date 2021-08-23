bb975ca39009fb17af7bc7b56aedf76073def8c5
Khusan Juraev <56759913+KhusanJuraev@users.noreply.github.com>
2021-08-23 17:10:28 +0500

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,7 +34,6 @@ function generateBarcodes() {
                 format: barcode_type,
                 lineColor: '#000',
                 displayValue: show_text,
-                width: 3,
                 fontSize: 28
             })
         })
